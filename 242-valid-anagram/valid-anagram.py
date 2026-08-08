1ce7028fe545064b67f20c406c368a72423a6378
Anish Talluri <anisht@outlook.com>
2026-08-07 17:42:35 -0700

    Time: 11 ms (77.27%) | Memory: 19.4 MB (46.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,12 +3,12 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        count = [0] * 26
+        arr = [0] * 26
 
-        for char in s:
-            count[ord(char) - ord('a')] += 1
+        for c in s:
+            arr[ord(c) - ord('a')] += 1
+
+        for c in t:
+            arr[ord(c) - ord('a')] -= 1
         
-        for char in t:
-            count[ord(char) - ord('a')] -= 1
-        
-        return all(x == 0 for x in count)
+        return all(x == 0 for x in arr)
