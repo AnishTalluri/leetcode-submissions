c97c7043455ec946e517c47f97713bba18bd9832
Anish Talluri <anisht@outlook.com>
2026-08-11 00:38:22 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (72.85%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -5,14 +5,6 @@ class Solution:
 
         prefix = strs[0]
 
-        # for string in strs[1:]:
-        #     i = 0
-        #     while i < len(string) and i < len(prefix) and prefix[i] == string[i]:
-        #         i += 1
-        #     prefix = prefix[:i]
-        #     if not prefix:
-        #         return ""
-
         for string in strs[1:]:
             while not string.startswith(prefix):
                 prefix = prefix[:-1]
